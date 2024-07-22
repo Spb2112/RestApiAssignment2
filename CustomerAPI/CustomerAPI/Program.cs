b9725bbf72c220168a2ce8065f98210668776749
Spb2112 <155693565+Spb2112@users.noreply.github.com>
2024-07-21 20:51:59 -0600

    Update Program.cs

--- CustomerAPI/CustomerAPI/Program.cs
+++ CustomerAPI/CustomerAPI/Program.cs
@@ -8,55 +8,38 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CustomerAPI
 {
-    public class Program
+    public class Mapping : Profile
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            
-            builder.Services.AddDbContext<DataContext>(options =>
-                options.UseInMemoryDatabase("InMemoryDb"));
-
-            builder.Services.AddControllers();
-            builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
-
-            
-            builder.Services.AddScoped<ICustomerRepo, CustomerRepo>();
-            builder.Services.AddScoped<ICustomerService, CustomerService>();
-
-            
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
-    public class MappingProfile : Profile
-    {
-        public MappingProfile()
+        public Mapping()
         {
             CreateMap<Customer, CustomerDto>().ReverseMap();
         }
     }
 
-
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);            
+            builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
+            builder.Services.AddControllers();
+            builder.Services.AddAutoMapper(typeof(Mapping).Assembly);            
+            builder.Services.AddScoped<ICustomerRepo, CustomerRepo>();
+            builder.Services.AddScoped<ICustomerService, CustomerService>();            
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            var app = builder.Build();            
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    } 
 }
 
 
